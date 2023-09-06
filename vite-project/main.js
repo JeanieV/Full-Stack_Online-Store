96dec1bdc12b2d2d681f8af1310c4a2e06bb0a7d
JeanieV <115072704+JeanieV@users.noreply.github.com>
2023-09-06 18:17:44 +0200

    About & Contact Us
    About & Contact Us design added

--- vite-project/main.js
+++ vite-project/main.js
@@ -9,3 +9,25 @@ const useData = async () => {
 const data = useData();
 console.log(data);
 
+
+// Get the button:
+let backToTop = document.getElementById("backToTopButton");
+
+backToTop.addEventListener("click", topFunction);
+
+// When the user scrolls down 80px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.documentElement.scrollTop > 300) {
+    backToTop.style.display = "block";
+  } else {
+    backToTop.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.documentElement.scrollTop = 800; // For Chrome, Firefox, IE and Opera
+}
+
---
+++ vite-project/src/js/aboutUs.js
@@ -0,0 +1,21 @@
+// Get the button:
+let backToTop = document.getElementById("backToTopButton");
+
+backToTop.addEventListener("click", topFunction);
+
+// When the user scrolls down 80px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.documentElement.scrollTop > 80) {
+    backToTop.style.display = "block";
+  } else {
+    backToTop.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.documentElement.scrollTop = 40; // For Chrome, Firefox, IE and Opera
+}
+
